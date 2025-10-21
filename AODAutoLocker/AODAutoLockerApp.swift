97d1e59b7b04e4ddf08205cfaaec2c6f774f9f33
Cosmo Zhang <zjzxz3006@126.com>
2025-10-21 16:42:47 +0800

    Initial Commit

---
+++ AODAutoLocker/AODAutoLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AODAutoLockerApp.swift
+//  AODAutoLocker
+//
+//  Created by Mac出租 on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AODAutoLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
